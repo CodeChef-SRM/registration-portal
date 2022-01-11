9ff93508e31fd3cfcb3a06dbc882931d0e78e0f4
Naman <namansharmatcr@gmail.com>
2022-01-11 12:45:31 +0530

    Added Popups to team page

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -7,18 +7,30 @@ import Heading from "../../components/Heading/Heading";
 import Button from "../../components/Button/Button";
 import List from "../../components/List/List";
 import welcomePage_data from "../../assets/js/welcome";
-import NewTeamMember from "../../popups/NewTeamMember/NewTeamMember";
+import InputField from "../../components/InputField/InputField";
+
 import "./Team.css";
 
 const Team = () => {
-  const [popUp, setState] = useState(false);
-  const togglePopUp = () => {
-    setState(!popUp);
-  }
-
+  
   const [mobNavbar, showMobNavbar] = useState(false);
   function setMobNavbar() {
     showMobNavbar(!mobNavbar);
+  }
+
+  const [addMember, setState] = useState(false);
+  const toggleAddMember = () => {
+    setState(!addMember);
+  }
+  
+  const [editMember, setEditMember] = useState(false);
+  const toggleEditMember = () => {
+    setEditMember(!editMember);
+  }
+
+  const [editTeam, setEditTeam] = useState(false);
+  const toggleEditTeam = () => {
+    setEditTeam(!editTeam);
   }
 
   return (
@@ -35,32 +47,73 @@ const Team = () => {
           <GrClose className='welcome-navbar-icon' onClick={setMobNavbar} style={{display: mobNavbar ? "inline" : "none"}}/>
         </div>
         <div className="team-container-left">
-          <Link to="/teamDetails"><Button type="Submit" text="Settings"/></Link>
+          <div className="submit-btn-div">
+            <Button onclick={toggleEditTeam} type="Submit" text="Settings"/>
+          </div>
           <Link to="/home"><Button type="Submit" text="Log Out"/></Link>
         </div>
 
       </div>
 
       <div className="mob-navbar" style={{display: mobNavbar ? "flex" : "none"}}>
-        <Link to="/teamDetails"><Button type="Submit" text="Settings"/></Link>
+          <div className="submit-btn-div">
+            <Button onclick={toggleEditTeam} type="Submit" text="Settings"/>
+          </div>
         <Link to="/home"><Button type="Submit" text="Log Out"/></Link>
       </div>
+
+      {/* ADD MEMBER */}
+
+      <div className={addMember ? "master-div" : "d-none"}>
+          <div className="main">
+          <div className="top">
+            <Heading text="Add Member"/> 
+            <GrClose onClick={toggleAddMember} className='close-icon'/>
+          </div>
+          <InputField type="text" name="Name" placeholder="Enter name"/>  
+          <InputField type="email" name="email" placeholder="Enter email ID"/>
+          <InputField type="text" name="regno" placeholder="Enter Reg. No"/>
+          <Link to="/team"><Button onclick={toggleAddMember} type="Submit" text="Add member"/></Link>
+        </div>
+      </div>
+
+      {/* EDIT MEMBER DETAILS */}
+
+      <div className={editMember ? "master-div" : "d-none"}>
+        <div className="main">
+        <div className="top">
+          <Heading text="Edit Member details"/> 
+          <GrClose onClick={toggleEditMember} className='close-icon'/>
+        </div>
+        <InputField type="text" name="Name" placeholder="Enter name"/>  
+        <InputField type="email" name="email" placeholder="Enter email ID"/>
+        <InputField type="text" name="regno" placeholder="Enter Reg. No"/>
+        <Button onclick={toggleEditMember} type="Submit" text="Update details"/>
+      </div>
+    </div>
+
+    {/* EDIT TEAM DETAILS */}
+    <div className={editTeam ? "master-div" : "d-none"}>
+        <div className="main">
+        <div className="top">
+          <Heading text="Edit Team details"/> 
+          <GrClose onClick={toggleEditTeam} className='close-icon'/>
+        </div>
+        <InputField type="text" name="Name" placeholder="Enter team name"/>  
+        <InputField type="email" name="email" placeholder="Enter password"/>
+        <Button onclick={toggleEditTeam} type="Submit" text="Update details"/>
+      </div>
+    </div>
 
       <h3 className="team-members">Team Members</h3>
       
       {/* TEAM LIST */}
 
       { welcomePage_data.map((listData, index) => {
-        return <List key={index} name={listData.name} email={listData.email} regno={listData.regno}/>
+        return <List onclick={toggleEditMember} key={index} name={listData.name} email={listData.email} regno={listData.regno}/>
       })}
 
-      <Link to="/addMember"><Button onClick={togglePopUp} className="add-member-btn" type="Submit" text="Add Member"/></Link>
-
-      {popUp && (
-        <div className="popup">
-          <NewTeamMember />
-        </div>
-      )}
+      <Button onclick={toggleAddMember} className="add-member-btn" type="Submit" text="Add Member"/>
     </div>
   )
 }
